43ca8518dd15e1cd1195ba39393a30df32d39e6e
Lokusok <lokusokdrobk@gmail.com>
2025-02-27 18:40:20 +0300

    Update pagination

--- resources/views/pagination/default.blade.php
+++ resources/views/pagination/default.blade.php
@@ -2,7 +2,15 @@
     <div class="flex justify-center gap-x-[10px]">
         @foreach ($elements[0] as $page => $link)
             <div>
-                <a class="px-3 py-2 rounded-lg shadow-md cursor-pointer active:opacity-60 hover:bg-gray-100" href="{{ $link }}">{{ $page }}</a>
+                <a
+                    @class([
+                        "px-3 py-2 rounded-lg shadow-md cursor-pointer active:opacity-60 hover:bg-gray-100",
+                        "bg-gray-200 opacity-50 pointer-events-none" => $paginator->currentPage() == $page
+                    ])
+                    href="{{ $link }}"
+                >
+                    {{ $page }}
+                </a>
             </div>
         @endforeach
     </div>
