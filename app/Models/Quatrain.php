95f91e525712587bae5f23d74aa34340c011818b
Lokusok <lokusokdrobk@gmail.com>
2025-02-27 18:29:18 +0300

    Remove xss fully

--- app/Models/Quatrain.php
+++ app/Models/Quatrain.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use HTMLPurifier;
+use HTMLPurifier_Config;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -12,5 +15,14 @@ class Quatrain extends Model
     public function song(): BelongsTo
     {
         return $this->belongsTo(Song::class);
+    }
+
+    public function body(): Attribute
+    {
+        $purifier = new HTMLPurifier(HTMLPurifier_Config::createDefault());
+
+        return Attribute::make(
+            get: fn (string $value) => $purifier->purify($value),
+        );
     }
 }
